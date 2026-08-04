4a342a61254f1cddbe5d630ec88aea0cf44dd4b4
fortblocks <chris@ridiculouscorp.com>
2026-08-04 17:53:55 +0100

    Add minimal interaction script

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function () {
+  const nav = document.getElementById('nav');
+  function onScroll() {
+    if (window.scrollY > 40) nav.classList.add('scrolled');
+    else nav.classList.remove('scrolled');
+  }
+  window.addEventListener('scroll', onScroll, { passive: true });
+  onScroll();
+})();
